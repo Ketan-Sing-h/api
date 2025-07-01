2f613ee0c3e44f39c9844ce4a4faf08bfe6f2ad9
Ketan-Sing-h <ketansinghldh@gmail.com>
2025-07-01 11:59:33 +0530

    changes made

--- api_practice.py
+++ api_practice.py
@@ -4,5 +4,5 @@ url = 'https://dummyjson.com/products/1'
 response = requests.get(url, verify=False)
 
 data = response.json()
-data = data['id','title','category']
+# data = data['id','title','category']
 print(data)
